71f25c9ebdd63c0c08746a0d0f0ab9acae5b0b67
Rutger Taal <rutger.taal@gmail.com>
2018-06-02 15:14:57 +0900

    create signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
   root to: 'tasks#index'
   resources:tasks
+  resources :users
   end
 
   get "*path", to: redirect("/#{I18n.default_locale}/%{path}")
   get "", to: redirect("/#{I18n.default_locale}")
+  
 end

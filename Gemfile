790ee475d7d8d39e66f9ecc70c3473765bc430be
Rutger Taal <rutger.taal@gmail.com>
2018-06-06 22:36:18 +0900

    implement basic login

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'rubocop'
 gem 'kaminari'
+gem 'jquery-rails'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
   root to: 'tasks#index'
-  resources:tasks
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :tasks
   resources :users
   end
 
